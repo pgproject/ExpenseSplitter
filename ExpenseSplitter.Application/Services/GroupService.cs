f7db52159f2f3052d83c4db84385d0dffd598b83
Grzegorz <grzp91@gmail.com>
2026-08-06 16:38:54 +0200

    update usings

--- ExpenseSplitter.Application/Services/GroupService.cs
+++ ExpenseSplitter.Application/Services/GroupService.cs
@@ -3,7 +3,8 @@ using ExpenseSplitter.Application.Interfaces.Groups;
 using ExpenseSplitter.Application.Interfaces.Users;
 using ExpenseSplitter.Domain.Entities;
 using ExpenseSplitter.Domain.Entities.Groups;
-using ExpenseSplitter.Domain.Exceptions;
+using ExpenseSplitter.Domain.Exceptions.Group;
+using ExpenseSplitter.Domain.Exceptions.User;
 
 namespace ExpenseSplitter.Application.Services
 {
